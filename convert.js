c84bcee4fdba70e52cf199ac57894b34c8262fbe
tolechowski1 <82466333+tolechowski1@users.noreply.github.com>
2022-01-25 19:44:43 -0500

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,6 @@
+
+let f = prompt("Enter Fahrenheit temperature");
+f = parseInt(f);
+let c = (f-32)* 5/9;
+c = parseInt(c);
+alert("Fahrenheit temperature is "+ f + "/n/n Celcius temperature is "+c);
